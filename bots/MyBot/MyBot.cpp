d3d03d39d2bc358bd764ba39e07ade2d15037d29
Sujeet Gholap <sujeetgholap@gmail.com>
2012-11-12 19:49:28 +0530

    added minimax. 5 ply. Still very basic eval function. no pruning.

--- bots/MyBot/MyBot.cpp
+++ bots/MyBot/MyBot.cpp
@@ -1,79 +1,162 @@
 /*
-* @file botTemplate.cpp
-* @author Arun Tejasvi Chaganty <arunchaganty@gmail.com>
-* @date 2010-02-04
-* Template for users to create their own bots
-*/
+ * @file botTemplate.cpp
+ * @author Arun Tejasvi Chaganty <arunchaganty@gmail.com>
+ * @date 2010-02-04
+ * Template for users to create their own bots
+ */
 
 #include "Othello.h"
 #include "OthelloBoard.h"
 #include "OthelloPlayer.h"
 #include <cstdlib>
+
+#define MIN -1
+#define MAX 1
+#define OPPOSITE(X) -X
+// #define print_move(move) cout << (move).x << " " << (move).y << endl;
+#define print_move(move) 0;                     \
+
+#define MAX_DEPTH 5
+
 using namespace std;
 using namespace Desdemona;
 
+class MinimaxResult 
+{
+public :
+     Move move;
+     int value;
+     MinimaxResult (const Move & move, int value) : move (0,0)
+          {
+               this->move = move;
+               this->value = value;
+          }
+};
+
 class MyBot: public OthelloPlayer
 {
-    public:
-        /**
-         * Initialisation routines here
-         * This could do anything from open up a cache of "best moves" to
-         * spawning a background processing thread. 
-         */
-        MyBot( Turn turn );
+public:
+     /**
+      * Initialisation routines here
+      * This could do anything from open up a cache of "best moves" to
+      * spawning a background processing thread. 
+      */
+     MyBot( Turn turn );
 
-        /**
-         * Play something 
-         */
-        virtual Move play( const OthelloBoard& board );
-    private:
+     /**
+      * Play something 
+      */
+     virtual Move play( const OthelloBoard& board );
+private:
+     int evaluate (const OthelloBoard & board) ;
+     MinimaxResult mini_max (const OthelloBoard & board,
+                             int min_or_max,
+                             int depth,
+                             Turn turn);
 };
 
 MyBot::MyBot( Turn turn )
-    : OthelloPlayer( turn )
+     : OthelloPlayer( turn )
 {
+}
+
+int MyBot::evaluate (const OthelloBoard & board) 
+{
+     int score = (board.getBlackCount ()
+                  - board.getRedCount ());
+     if (this->turn == RED) {
+          score = -score;
+     }
+     return score;
+}
+
+// min_or_max : can be either MIN or MAX
+//              MIN -> From the child minmax values choose min
+//              MAX -> From the child minmax values choose max
+// depth      : When 0, return the evaluation function value for board
+//              instead of returning 'best' minimax value for the children.
+// turn       : Note that this might be different from this->turn
+//              this parameter denotes at this position of the board,
+//              who is going to make next turn.
+MinimaxResult MyBot::mini_max (const OthelloBoard & board,
+                               int min_or_max,
+                               int depth,
+                               Turn turn) 
+{
+     // Leaf node due to depth.
+     if (depth == 0) {
+          Move move (0, 0); // has no consequences.
+          return MinimaxResult (move, this->evaluate (board));
+     }
+
+
+     list<Move> moves = board.getValidMoves (turn);
+     list<Move>::iterator it = moves.begin();
+
+     // Leaf node due to unavailability of moves.
+     if (moves.size () == 0) {
+          Move move (0, 0); // has no consequences.
+          return MinimaxResult (move, this->evaluate (board));
+     }
+     
+     OthelloBoard test_board (board);
+     test_board.makeMove (turn, *it);
+     int best_minimax_value_so_far = 
+          this->mini_max (test_board,
+                          OPPOSITE (min_or_max),
+                          depth - 1,
+                          other (turn)).value;
+     Move best_move = *it;
+     ++ it;
+    
+     for (; it != moves.end(); ++it) {
+          OthelloBoard test_board (board);
+          test_board.makeMove (turn, *it);
+          int minimax_value = this->mini_max (test_board,
+                                              OPPOSITE (min_or_max),
+                                              depth - 1,
+                                              other (turn)).value;
+ 
+          switch (min_or_max) {
+          case MIN :
+               if (minimax_value < best_minimax_value_so_far) {
+                    best_minimax_value_so_far = minimax_value;
+                    best_move = *it;
+               }
+               break;
+          case MAX :
+               if (minimax_value > best_minimax_value_so_far) {
+                    best_minimax_value_so_far = minimax_value;
+                    best_move = *it;
+               }
+               break;
+          }
+     }
+     return MinimaxResult (best_move, best_minimax_value_so_far);
 }
 
 Move MyBot::play( const OthelloBoard& board )
 {
-    list<Move> moves = board.getValidMoves( turn );
-    list<Move>::iterator it = moves.begin();
-    if (this->turn == BLACK) cout << "black\n";
-    if (this->turn == RED) cout << "red\n";
-    
-    Move final_move = *it;
-    int max_coin_count = 0;
-    for (; it != moves.end(); ++it) {
-         // Simulate a move, and choose which maximizes your coin count.
-         OthelloBoard test_board (board);
-         test_board.makeMove (this->turn, *it);
-
-         int count = 0;
-         if (this->turn == BLACK) count = test_board.getBlackCount ();
-         if (this->turn == RED) count = test_board.getRedCount ();
-
-         if (count > max_coin_count) {
-              max_coin_count = count;
-              final_move = *it;
-         }
-    }
-    
-
-    return final_move;
+     MinimaxResult result = this->mini_max (board,
+                                            MAX,
+                                            MAX_DEPTH,
+                                            this->turn);
+     return result.move;
 }
+
 
 // The following lines are _very_ important to create a bot module for Desdemona
 
 extern "C" {
-    OthelloPlayer* createBot( Turn turn )
-    {
-        return new MyBot( turn );
-    }
+     OthelloPlayer* createBot( Turn turn )
+     {
+          return new MyBot( turn );
+     }
 
-    void destroyBot( OthelloPlayer* bot )
-    {
-        delete bot;
-    }
+     void destroyBot( OthelloPlayer* bot )
+     {
+          delete bot;
+     }
 }
 
 
